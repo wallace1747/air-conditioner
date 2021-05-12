f91e725f6213874b583eee7aabd4a6c28fe55396
Wallace you <wallace1747@gmail.com>
2021-05-12 23:06:08 +0800

    Update AirConditioner.tsx

--- src/features/ac/AirConditioner.tsx
+++ src/features/ac/AirConditioner.tsx
@@ -256,7 +256,7 @@ function EnergySavingLabel() {
       >
         <img
           className="ximalaya-logo"
-          src="/ximalaya-logo.png"
+          src="/*/ximalaya-logo.png*/"
           alt="ximalaya-logo"
         />
       </a>
