33728862c0fadb33270f35e8f7bde0c95aeb4ee2
Wallace you <wallace1747@gmail.com>
2021-05-12 23:56:43 +0800

    Update AirConditioner.tsx

--- src/features/ac/AirConditioner.tsx
+++ src/features/ac/AirConditioner.tsx
@@ -310,7 +310,7 @@ export default function AirConditioner(props: {
         <AirOutlet />
         <AcStatus status={props.status} />
         <EnergyLabel className={classes.energyLabel} titleLength={6} />
-        {process.env.REACT_APP_DISABLE_ADSENSE ? null : <EnergySavingLabel />}
+        {process.env.REACT_APP_DISABLE_ADSENSE ? <EnergySavingLabel /> : null}
       </AcBorder>
       <Fade in={props.status} timeout={{ enter: 2500, exit: 1500 }}>
         <WindEffect />
