9f3cbd2311b38bc909093af12d2645703d14603b
Wallace you <wallace1747@gmail.com>
2021-05-14 08:58:50 +0800

    Update index.ts

--- src/features/adsense/index.ts
+++ src/features/adsense/index.ts
@@ -4,7 +4,7 @@ import ReactGA from "react-ga";
  * 喜马拉雅广告链接
  */
 export const ximalayaLink =
-  "https://www.baidu.com";
+  "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.51yuansu.com%2Fpic3%2Fcover%2F02%2F84%2F03%2F5a5c898b2a644_610.jpg&refer=http%3A%2F%2Fpic.51yuansu.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1623545475&t=29ace2b00fa96c52a52f17055a2353e5";
 
 /**
  * 跳转至喜马拉雅
